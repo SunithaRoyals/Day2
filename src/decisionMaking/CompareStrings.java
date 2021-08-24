af01b39966ebf59c4cd7f911f7348ea9c0720db7
SunithaRoyals <sunitharoyals97@gmail.com>
2021-08-24 19:35:15 +0530

    first commit

---
+++ src/decisionMaking/CompareStrings.java
@@ -0,0 +1,25 @@
+package decisionMaking;
+import java.util.Scanner;
+
+public class CompareStrings {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		if(a>b)
+		{
+			System.out.println(a+ "is greater than" +b);
+		}
+		else if(a<b)
+		{
+			System.out.println(a+ "is less than" +b);
+		}
+		else
+		{
+			System.out.println(a+ "is equal to" +b);
+		}
+	}
+
+}
---
+++ src/decisionMaking/VowelConsonant.java
@@ -0,0 +1,27 @@
+package decisionMaking;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) {
+		char ch;
+		Scanner sc= new Scanner(System.in);
+		ch = sc.next().charAt(0);
+		if(ch=='a'|| ch=='e'|| ch=='i'||ch=='o'||ch=='u')
+		{
+			System.out.println("Vowel");
+		
+		}
+		else if(!(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'))
+		{
+			System.out.println("Consonants");
+		}
+		else
+		{
+			System.out.println("Not a Alphabet");
+		}
+
+	}
+
+}
